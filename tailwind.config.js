84f2ce2d1c95da09658fb3fdf1b52979954ec4bd
mitrasheamuskhanal <mitrasheamuskhanal@gmail.com>
2024-07-21 23:30:27 +0545

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./*.{html,php}",
+    "./path/to/your/templates/**/*.html",
+    "./path/to/your/templates/**/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
