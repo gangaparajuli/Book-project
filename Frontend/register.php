5aeb304db56ef0dd7fd34e82711b2caab6a6fb89
gangaparajuli <parajuliganga44@gmail.com>
2024-07-26 18:59:22 +0545

    book

--- Frontend/register.php
+++ Frontend/register.php
@@ -1,5 +1,8 @@
 <?php
+//include config file contains database connection details.
 include 'config.php';
+
+//Initialize an empty array to hold message tht will be shown to the user.
 $message = [];
 
 if(isset($_POST['submit'])){
@@ -9,21 +12,34 @@ if(isset($_POST['submit'])){
     $confirm_password = mysqli_real_escape_string($conn, md5($_POST['confirm-password']));
     $user_type = $_POST['user-type'];
 
-    $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('query failed');
 
-    if(mysqli_num_rows($select_users) > 0){
-        $message[] = 'User already exists!';
-    } else {
+    //query the database to check if a user with the given username and password already exists.
+    $select_users_username = mysqli_query($conn, "SELECT * FROM `users` WHERE name = '$username'") or die('query failed');
+
+    $select_users_password = mysqli_query($conn, "SELECT * FROM`users` WHERE password = '$password'") or die('query failed');
+
+    //check if the username already exists.
+    if(mysqli_num_rows($select_users_username) > 0){
+        $message[] = 'User with this username already exists!';
+
+        //check if the password already exists.
+    }elseif(mysqli_num_rows($select_users_password)>0){
+        $message[] = 'User with this password already exists!';
+    } 
+    else {
         if($password != $confirm_password){
             $message[] = 'Confirm password does not match!';
         } else {
+            //insert the new user into the database.
             mysqli_query($conn, "INSERT INTO `users`(`name`, `email`, `password`, `user_type`) VALUES ('$username', '$email', '$password', '$user_type')") or die('query failed');
             $message[] = 'Signup successful!';
+            //redirect to the login page.
             header('location:login.html');
         }
     }
 }
 
+//if there is any message, display to the user.
 if(!empty($message)){
     foreach($message as $msg){
         echo '
--- login.php
+++ login.php
@@ -1,39 +1,39 @@
 <?php
 
 include 'config.php';
-
 session_start();
+
 if(isset($_POST['submit'])){
-    $name = mysqli_real_escape_string($conn, $_POST['name']);
-    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $username = mysqli_real_escape_string($conn, $_POST['name']);
+    $password = mysqli_real_escape_string($conn, md5($_POST['password']));
+   // $email = mysqli_real_escape_string($conn, $_POST['email']);
     //$cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
     //$user_type =$_POST['user_type'];
 
-    $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE name ='$name'  AND password= '$pass'")
-    or die('query failed');
+    $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE name ='$username'  AND password = '$password'") or die('query failed');
 
     if(mysqli_num_rows($select_users) > 0){
 
     $row = mysqli_fetch_assoc($select_users);
 if($row['user_type'] == 'admin'){
     $_SESSION['admin_name'] = $row['name'];
-    $_SESSION['admin_email'] = $row['email'];
+    $_SESSION['admin_password'] = $row['password'];
+    //$_SESSION['admin_email'] = $row['email'];
     //$_SESSION ['admin_password'] =$row['password'];
     $_SESSION['admin_id'] = $row['id'];
-    header('location:admin_page.php');
+    header('location:admin_page.html');
 }
 elseif($row['user_type'] == 'Rentee'){
     $_SESSION['renter_name'] = $row['name'];
-    $_SESSION['renter_email'] = $row['email'];
-    //$_SESSION ['renter_password'] =$row['password'];
+    //$_SESSION['renter_email'] = $row['email'];
+    $_SESSION ['renter_password'] =$row['password'];
     $_SESSION['rentee_id'] = $row['id'];
 header('location:home.html');
 }
 elseif($row['user_type'] == 'Renter'){
    $_SESSION['rentee_name'] = $row['name'];
-   $_SESSION['rentee_email'] = $row['email'];
-    //$_SESSION ['rentee_password'] =$row['password'];
+  // $_SESSION['rentee_email'] = $row['email'];
+    $_SESSION ['rentee_password'] =$row['password'];
     $_SESSION['renter_id'] = $row['id'];
 header('location: home.html');
 }
